df36a11068a1584f3e1d68dbbd1945ee04e1af0e
stephcstd <markstephencustodio17@gmail.com>
2021-06-18 18:29:07 +0800

    Integrated database to orders and onvoices tab

--- frontend/src/components/OrdersTab.jsx
+++ frontend/src/components/OrdersTab.jsx
@@ -1,57 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useQuery } from "react-query";
 import axios from "axios";
 
-export default function OrdersTabData() {
-  const { data: res, isLoading } = useQuery(["orders"], async () => {
+export default function ProductsTabData() {
+  const { data, isLoading } = useQuery(["order"], async () => {
     return await axios
-      .get("/api/order/")
+      .get("/api/order")
       .then((res) => res.data)
       .catch((err) => console.error(err));
   });
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  if (isLoading) return <div>Loading...</div>;
+
+  function simplify(string){
+    if(string == "false") {
+      return "No";
+    }
+    else return "Yes";
+  }
+
   return (
     <div>
-      <h1>ORDERS</h1>
-      {!isLoading &&
-        res.map((order) => (
-          <div key={order._id} className="row">
-            <div className="column">
-              <p>{order._id}</p>
-            </div>
-            {/* <div className="column">
-              <p>{order.name}</p>
-            </div>
-            <div className="column">
-              <p>{order.category}</p>
-            </div>
-            <div className="column">
-              <p>{order.brand}</p>
-            </div>
-            <div className="column">
-              <p>{order.price}</p>
-            </div>
-            <div className="column">
-              <p>{order.countInStock}</p>
-            </div> */}
-            <div className="column" id="action-padding">
-              {/* <Link to={`/editorder/${order._id}`}>
-                <button id="edit-button" className="outline-primary">
-                  edit
-                </button>
-              </Link> */}
-              <button
-                id="delete-button"
-                className="outline-primary"
-                onClick={async () => {
-                  await axios.delete(`/api/orders/${order._id}/delete`);
-                }}
-              >
-                delete
-              </button>
-            </div>
+      <h2>{data && data.message}</h2>
+      <h1 className="ProductTabTitle">ORDERS</h1>
+      <div className="table">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+        </div>
+        <div className="row">
+          <div className="column">
+            <h3>Order ID</h3>
           </div>
-        ))}
+          <div className="column">
+            <h3>Product Name</h3>
+          </div>
+          <div className="column">
+            <h3>Total Price</h3>
+          </div>
+          <div className="column">
+            <h3>Paid</h3>
+          </div>
+          <div className="column">
+            <h3>Delivered</h3>
+          </div>
+        </div>
+        <tbody>
+          {data
+            /*.filter((order) => {
+              if (searchTerm === "") {
+                return order;
+              } else if (
+                order.name.toLowerCase().includes(searchTerm.toLowerCase())
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase())
+              ) {
+                return order;
+              }
+            })*/
+            .map((order, index) => (
+              <div key={index} className="row">
+                <div className="column">
+                  <p>{order._id}</p>
+                </div>
+                <div className="column">
+                  <p>{order.orderItems[0].name}</p>
+                </div>
+                <div className="column">
+                  <p>{order.totalPrice}</p>
+                </div>
+                <div className="column">
+                  <p>{simplify(order.isPaid.toString())}</p>
+                </div>
+                <div className="column">
+                  <p>{simplify(order.isDelivered.toString())}</p>
+                </div>
+              </div>
+            ))}
+        </tbody>
+      </div>
     </div>
   );
 }
